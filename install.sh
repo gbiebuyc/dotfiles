10cc3b26d4b5f2f3d089465edb54c27be383722d
gbiebuyc <gbiebuyc@users.noreply.github.com>
2020-02-05 14:06:15 +0100

    Fix install script for bash on windows

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 ln -sv ~/dotfiles/.vimrc ~
 ln -sv ~/dotfiles/.vim ~
 ln -sv ~/dotfiles/.screenrc ~
 ln -sv ~/dotfiles/.gitconfig ~
 line='source ~/dotfiles/.bashrc' 
-if [ $(uname) = "Darwin" ]
-then
+if [ "$(uname)" = "Darwin" ]; then
     echo "$line" > ~/.bash_profile
     echo "$line" > ~/.bashrc
-elif [ $(uname) = "Linux" ]
-then
+elif [ "$(uname)" = "Linux" ]; then
     grep -qxF "$line" ~/.bashrc || echo "$line" >> ~/.bashrc
+elif [[ "$(uname)" == *"MINGW"* ]]; then
+    echo "$line" > ~/.bashrc
 fi
